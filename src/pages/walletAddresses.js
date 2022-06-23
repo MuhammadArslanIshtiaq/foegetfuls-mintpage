11b8646849d594c7962345950746afb0475ad533
Muhammad Arslan <muhammadarslanishtiaq@gmail.com>
2022-06-23 14:44:29 +0500

    set feedback  updated

--- src/pages/walletAddresses.js
+++ src/pages/walletAddresses.js
@@ -1,6 +1,7 @@
 const whitelistAddresses = [
   "0x109050257f33b183EFe906a6Bb2Ad6993e4AB8b5",
   "0xF7a43B0Fb2C5d6700a63659b248b3921546F901A",
+  "0xcAfc2Dae33396659D977E1A7d9Daf757288b09e1",
 ];
 
 export default whitelistAddresses;
